0a669a37ff708107a0dff240ca9593ed20eb876c
Dominik <domix852@gmail.com>
2020-08-18 12:28:34 +0200

    Finished Register Activity

--- app/src/main/java/com/example/mybank/Authentication/RegisterActivity.java
+++ app/src/main/java/com/example/mybank/Authentication/RegisterActivity.java
@@ -2,6 +2,8 @@ package com.example.mybank.Authentication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
@@ -12,9 +14,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.mybank.Database.DatabaseHelper;
+import com.example.mybank.MainActivity;
+import com.example.mybank.Models.User;
 import com.example.mybank.R;
+import com.example.mybank.Utils;
 
 public class RegisterActivity extends AppCompatActivity {
     private static final String TAG = "RegisterActivity";
@@ -121,6 +127,124 @@ public class RegisterActivity extends AppCompatActivity {
             {
                 txtWarning.setVisibility(View.GONE);
                 
+            }
+        }
+    }
+
+    private class RegisterUser extends AsyncTask<Void,Void, User>
+    {
+        private String email;
+        private String password;
+        private String address;
+        private String first_name;
+        private String last_name;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            String email = edtTxtEmail.getText().toString();
+            String password = edtTxtPassword.getText().toString();
+            String address = edtTxtAddress.getText().toString();
+            String name = edtTextName.getText().toString();
+
+            this.email = email;
+            this.password = email;
+            this.password = password;
+
+            String[] names = name.split(" ");
+            if(names.length >=1)
+            {
+                this.first_name = names[0];
+                for (int i = 1; i < names.length ; i++) {
+                    if(i > 1)
+                    {
+                        last_name += " " + names[i];
+                    }else
+                    {
+                        last_name += names[i];
+                    }
+                    
+                }
+            }
+            else
+            {
+                this.first_name = names[0];
+            }
+        }
+
+        @Override
+        protected User doInBackground(Void... voids) {
+            try {
+                SQLiteDatabase db = databaseHelper.getWritableDatabase();
+
+                ContentValues values = new ContentValues();
+                values.put("email", this.email);
+                values.put("password", this.password);
+                values.put("address", this.address);
+                values.put("first_name", this.first_name);
+                values.put("last_name", this.last_name);
+                values.put("remained_amount", 0.0);
+
+                long userId = db.insert("users", null, values);
+                Log.d(TAG, "doInBackground: userId");
+
+                Cursor cursor = db.query("users",null, "_id=?", new String[] {String.valueOf(userId)},
+                null,null,null);
+
+                if(null!= cursor)
+                {
+                    if(cursor.moveToFirst())
+                    {
+                        User user = new User();
+                        user.set_id(cursor.getInt(cursor.getColumnIndex("_id")));
+                        user.setEmail(cursor.getString(cursor.getColumnIndex("email")));
+                        user.setAddress(cursor.getString(cursor.getColumnIndex("address")));
+                        user.setPassword(cursor.getString(cursor.getColumnIndex("password")));
+                        user.setFirst_name(cursor.getString(cursor.getColumnIndex("first_name")));
+                        user.setLast_name(cursor.getString(cursor.getColumnIndex("last_name")));
+                        user.setRemained_amount(cursor.getDouble(cursor.getColumnIndex("remained_amount")));
+
+                        cursor.close();
+                        db.close();
+                        return user;
+                    }
+                    else
+                    {
+                        cursor.close();
+                        db.close();
+                        return null;
+                    }
+                }else
+                {
+                    db.close();
+                    return null;
+                }
+
+            }catch (SQLException x)
+            {
+                x.printStackTrace();
+                return null;
+            }
+
+        }
+
+        @Override
+        protected void onPostExecute(User user) {
+            super.onPostExecute(user);
+
+            if(null!= user)
+            {
+                Toast.makeText(RegisterActivity.this,"User " + user.getEmail() + " registered succesfully",Toast.LENGTH_SHORT).show();
+                Utils utils = new Utils(RegisterActivity.this);
+                utils.addUserToSharedPreferences(user);
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+
+            }
+            else
+            {
+                Toast.makeText(RegisterActivity.this, "Was not able to register, please try again", Toast.LENGTH_SHORT).show();
             }
         }
     }
