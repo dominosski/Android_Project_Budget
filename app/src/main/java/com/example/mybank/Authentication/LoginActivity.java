ecc20ecb92c7b093f639c76a2ad37a8066a55eff
Dominik <domix852@gmail.com>
2020-08-20 19:13:42 +0200

    Added main activity layout

--- app/src/main/java/com/example/mybank/Authentication/LoginActivity.java
+++ app/src/main/java/com/example/mybank/Authentication/LoginActivity.java
@@ -2,13 +2,23 @@ package com.example.mybank.Authentication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.mybank.Database.DatabaseHelper;
+import com.example.mybank.MainActivity;
+import com.example.mybank.Models.User;
 import com.example.mybank.R;
+import com.example.mybank.Utils;
 
 public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "LoginActivity";
@@ -17,6 +27,11 @@ public class LoginActivity extends AppCompatActivity {
     private EditText edtTxtEmail, edtTxtPassword;
     private Button btnLogin;
 
+    private DatabaseHelper databaseHelper;
+
+    private LoginUser loginUser;
+    private DoesEmailExist doesEmailExist;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +39,46 @@ public class LoginActivity extends AppCompatActivity {
 
         initViews();
 
+        txtRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                initLogin();
+            }
+        });
+
+    }
+
+    private void initLogin()
+    {
+        Log.d(TAG, "initLogin: started");
+
+        if(!edtTxtEmail.getText().toString().equals(""))
+        {
+            if(!edtTxtPassword.getText().toString().equals(""))
+            {
+                txtWarning.setVisibility(View.GONE);
+
+                doesEmailExist = new DoesEmailExist();
+                doesEmailExist.execute(edtTxtEmail.getText().toString());
+            }else
+            {
+                txtWarning.setVisibility(View.VISIBLE);
+                txtWarning.setText("Please enter your password");
+            }
+        }
+        else
+        {
+            txtWarning.setVisibility(View.VISIBLE);
+            txtWarning.setText("Please enter your email");
+        }
     }
 
     private void initViews()
@@ -36,5 +91,157 @@ public class LoginActivity extends AppCompatActivity {
 
         txtWarning = (TextView)findViewById(R.id.txtWarning);
         txtRegister = (TextView)findViewById(R.id.txtRegister);
+    }
+
+    private class DoesEmailExist extends AsyncTask<String, Void, Boolean>
+    {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            databaseHelper = new DatabaseHelper(LoginActivity.this);
+
+        }
+
+        @Override
+        protected Boolean doInBackground(String... strings) {
+            try {
+                SQLiteDatabase db = databaseHelper.getReadableDatabase();
+                Cursor cursor = db.query("users", new String[] {"email"}, "email=?", new String[]
+                        {strings[0]}, null, null, null);
+                if(null!= cursor)
+                {
+                    if(cursor.moveToFirst())
+                    {
+                        cursor.close();
+                        db.close();
+                        return true;
+                    }
+                    else
+                    {
+                        cursor.close();
+                        db.close();
+                        return false;
+                    }
+                }
+                else
+                {
+                    db.close();
+                    return false;
+                }
+            }
+            catch(SQLException x)
+            {
+                x.printStackTrace();
+                return false;
+            }
+
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            super.onPostExecute(aBoolean);
+
+            if(aBoolean)
+            {
+                loginUser = new LoginUser();
+                loginUser.execute();
+            }
+            else
+            {
+                txtWarning.setVisibility(View.VISIBLE);
+                txtWarning.setText("There is no such user with this email");
+            }
+        }
+    }
+
+    private class LoginUser extends AsyncTask<Void, Void, User>
+    {
+        private String email;
+        private String password;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            this.email = edtTxtEmail.getText().toString();
+            this.password = edtTxtPassword.getText().toString();
+        }
+
+        @Override
+        protected User doInBackground(Void... voids) {
+
+            try {
+                SQLiteDatabase db = databaseHelper.getReadableDatabase();
+                Cursor cursor = db.query("users", null, "email=? AND password=?",
+                        new String[] {email, password}, null, null, null);
+
+                if(null!=cursor)
+                {
+                    if(cursor.moveToFirst())
+                    {
+                        User user = new User();
+                        user.set_id(cursor.getInt(cursor.getColumnIndex("_id")));
+                        user.setEmail(cursor.getString(cursor.getColumnIndex("email")));
+                        user.setAddress(cursor.getString(cursor.getColumnIndex("address")));
+                        user.setPassword(cursor.getString(cursor.getColumnIndex("password")));
+                        user.setFirst_name(cursor.getString(cursor.getColumnIndex("first_name")));
+                        user.setLast_name(cursor.getString(cursor.getColumnIndex("last_name")));
+                        user.setRemained_amount(cursor.getDouble(cursor.getColumnIndex("remained_amount")));
+
+                        cursor.close();
+                        db.close();
+                        return user;
+                    }
+                }else
+                {
+                    return null;
+                }
+            }catch (SQLException x)
+            {
+                x.printStackTrace();
+                return null;
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(User user) {
+            super.onPostExecute(user);
+
+            if(null!= user)
+            {
+                Utils utils = new Utils(LoginActivity.this);
+                utils.addUserToSharedPreferences(user);
+
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }else
+            {
+                txtWarning.setVisibility(View.VISIBLE);
+                txtWarning.setText("Your password is incorrect");
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if(null!=doesEmailExist)
+        {
+            if(!doesEmailExist.isCancelled())
+            {
+                doesEmailExist.cancel(true);
+            }
+        }
+        if(null != loginUser)
+        {
+            if(!loginUser.isCancelled())
+            {
+                loginUser.cancel(true);
+            }
+        }
     }
 }
--- app/src/main/java/com/example/mybank/MainActivity.java
+++ app/src/main/java/com/example/mybank/MainActivity.java
@@ -1,53 +1,148 @@
 package com.example.mybank;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.NestedScrollView;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
+import com.example.mybank.Authentication.LoginActivity;
 import com.example.mybank.Authentication.RegisterActivity;
 import com.example.mybank.Database.DatabaseHelper;
 import com.example.mybank.Models.User;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.LineChart;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
     private Utils utils;
 
-    private Button btn;
+    private BottomNavigationView bottomNavigationView;
+    private TextView txtAmount, txtWelcome;
+    private RecyclerView transactionRecView;
+    private FloatingActionButton fbAddTransaction;
+    private Toolbar toolbar;
+    private BarChart barChart;
+    private LineChart lineChart;
+
+    private DatabaseHelper databaseHelper;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = (Button)findViewById(R.id.button);
-
         utils = new Utils(this);
         User user = utils.isUserLoggedIn();
+
+        initViews();
+        initBottomNavView();
+        setSupportActionBar(toolbar);
 
         if(null!=user)
         {
             Toast.makeText(this, "User: " + user.getFirst_name() + " logged in", Toast.LENGTH_SHORT).show();
         }else
         {
-            Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(intent);
         }
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        setupAmount();
+
+    }
+    private void setupAmount()
+    {
+        Log.d(TAG, "setupAmount: started");
+        User user = utils.isUserLoggedIn();
+        if(null != user)
+        {
+
+        }
+    }
+    private class GetAccountAmount extends AsyncTask<Integer, Void, Double>
+    {
+        @Override
+        protected Double doInBackground(Integer... integers) {
+            try {
+                SQLiteDatabase db = databaseHelper.getReadableDatabase();
+                Cursor cursor = db.query("users", new String[] {"remained_amount"}, "_id=?",
+                new String[] {String.valueOf(integers[0])}, null, null, null);
+                
+
+            }catch (SQLException x)
+            {
+                x.printStackTrace();
+            }
+            return null;
+        }
+    }
+
+    private void setSupportActionBar(Toolbar toolbar) {
+    }
+
+    private void initBottomNavView()
+    {
+        Log.d(TAG, "initBottomNavView: started");
+        bottomNavigationView.setSelectedItemId(R.id.menu_item_home);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                utils.signOutUser();
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId())
+                {
+                    case R.id.menu_item_stats:
+                    //TODO
+                    break;
+                    case R.id.menu_item_investment:
+
+                        break;
+                    case R.id.menu_item_loans:
+
+                        break;
+                    case R.id.menu_item_transaction:
+
+                        break;
+                    case R.id.menu_item_home:
+
+                        break;
+                }
+
+
+                return false;
             }
         });
+    }
 
+    private void initViews()
+    {
+        Log.d(TAG, "initViews: started");
+
+        transactionRecView = (RecyclerView)findViewById(R.id.transactionRecView);
+        txtAmount = (TextView)findViewById(R.id.txtAmount);
+        txtWelcome = (TextView)findViewById(R.id.txtWelcome);
+        barChart = (BarChart)findViewById(R.id.profitChart);
+        lineChart = (LineChart)findViewById(R.id.dailySpentChart);
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottomNavView);
+        fbAddTransaction = (FloatingActionButton)findViewById(R.id.fbAddTransaction);
     }
 }
